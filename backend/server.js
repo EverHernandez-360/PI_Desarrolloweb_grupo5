742da13b125a1dda521874bebbd3ec2d9595de7c
carlosp06 <3062414980311@ingenieria.usac.edu.gt>
2026-03-25 10:38:51 -0600

    Agregando configuracion del servidor y ocultando carpetas con gitignore

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require('./src/app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`URL: http://localhost:${PORT}`);
+});
